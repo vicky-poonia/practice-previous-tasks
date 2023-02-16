cc326246eafeff97770240539be443054f6ecf41
vicky-poonia <vickypoonia020@gmail.com>
2023-02-16 23:12:44 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Save to reload. </p>
         <a
-          className="App-link"
+          className="link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
